29b582d0ebfcf9abcdb3af59c9fba051661bf1d2
CarlosDC10 <carlosduatocdc@gmail.com>
2023-04-24 11:04:28 +0100

    23/4/23

--- models/linea_preparada.py
+++ models/linea_preparada.py
@@ -9,7 +9,7 @@ class LineaPreparadaModel(models.Model):
 
     lineaPedido = fields.Many2one(comodel_name="app_pedidos.linea_pedido",string="Linea pedido:")
     lote = fields.Integer(string="Lote: ",help="Cuantas unidades del mismo lote", default = int(str(date.today()-date(date.today().year,1,1))[0:3])+1)
-    cantidad = fields.Integer(string="Cantidad: ",help="Cuantas unidades del tipo de paquete", required = True, default = 1)
+    cantidad = fields.Integer(string="Cantidad: ",help="Cuantas unidades del tipo de paquete", default = 1)
     tipoPaquete = fields.Many2one(comodel_name="app_pedidos.tipo_paquete",string="Tipo paquete:",compute = "setTipoPaquete", store = True)
     completada = fields.Boolean(string="Completada?", default=False)
 
@@ -27,16 +27,3 @@ class LineaPreparadaModel(models.Model):
             raise ValidationError("Hay mas cantiddad de la necesaria. Compruebe el inventario")
         elif acum == self.lineaPedido.cantidad:
             self.completada = True
-
-    def anyadirUno(self):
-        for rec in self: 
-            rec.cantidad += 1
-            self.comprobarCantidad()
-            rec.tipoPaquete = rec.lineaPedido.tipoPaquete
-
-    def restarUno(self):
-        for rec in self: 
-            rec.cantidad -= 1
-            self.comprobarCantidad()
-            rec.tipoPaquete = rec.lineaPedido.tipoPaquete
-
